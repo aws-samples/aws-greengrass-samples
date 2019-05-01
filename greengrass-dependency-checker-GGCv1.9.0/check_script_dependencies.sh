0cb515309de9778b869d5bf74c81e52ff8d282f7
Jinbo Pan <panjinbo@amazon.com>
2019-05-01 00:38:44 +0000

    Add files for 1.9.0 dependency checker

---
+++ greengrass-dependency-checker-GGCv1.9.0/check_script_dependencies.sh
@@ -0,0 +1,474 @@
+## Commands assumed to be present on the device:
+## 1. echo
+## 2. exit
+## 3. set
+##
+## This script checks for shell or BusyBox variants of the following commands:
+## 1. printf
+## 2. uname
+## 3. cat
+## 4. ls
+## 5. head
+## 6. find
+## 7. zcat
+## 8. awk
+## 9. sed
+## 10. wc
+## 11. cut
+## 12. sort
+## 13. expr
+## 14. grep
+## 15. test
+## 16. dirname
+## 17. readlink
+## 18. xargs
+## 19. uniq
+## 20. strings
+## 21. id
+##
+## Shell commands not supported by Busybox and required to be present on the device:
+## 1. eval
+## 2. command
+## 3. read
+TEST_STRING="hello world\nHello World"
+TEST_FILE="$0"
+PATTERN="hello"
+
+PRINTF="printf"
+UNAME="uname"
+CAT="cat"
+LS="ls"
+HEAD="head"
+FIND="find"
+ZCAT="zcat"
+AWK="awk"
+SED="sed"
+WC="wc"
+CUT="cut"
+SORT="sort"
+EXPR="expr"
+GREP="grep"
+TEST="test"
+DIRNAME="dirname"
+READLINK="readlink"
+TR="tr"
+XARGS="xargs"
+STRINGS="strings"
+UNIQ="uniq"
+ID="id"
+
+DEPENDENCIES_PRESENT=1
+
+check_printf_present() {
+    {
+        ## Try the shell command first
+        printf "$TEST_STRING" 2>/dev/null 1>&2
+    } || {
+        {   ## Fall back to the BusyBox variant
+            busybox printf "$TEST_STRING" 2>/dev/null 1>&2
+        } && {
+            PRINTF="busybox printf"
+        }
+    } || {
+        wrap_bad "printf" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_eval_present() {
+    {
+        ## Busybox does not support 'eval'. Fail if 'eval' is not found.
+        eval 2>/dev/null 1>&2
+    } || {
+        wrap_bad "eval" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_uname_present() {
+    {
+        uname 2>/dev/null 1>&2
+    } || {
+        {
+            busybox uname 2>/dev/null 1>&2
+        } && {
+            UNAME="busybox uname"
+        }
+    } || {
+        wrap_bad "uname" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_cat_present() {
+    {
+        cat "$TEST_FILE" 2>/dev/null 1>&2
+    } || {
+        {
+            busybox cat "$TEST_FILE" 2>/dev/null 1>&2
+        } && {
+            CAT="busybox cat"
+        }
+    } || {
+        wrap_bad "cat" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_ls_present() {
+    {
+        ls "$TEST_FILE"  2>/dev/null 1>&2
+    } || {
+        {
+            busybox ls "$TEST_FILE"  2>/dev/null 1>&2
+        } && {
+            LS="busybox ls"
+        }
+    } || {
+        wrap_bad "ls" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_head_present() {
+    {
+        $PRINTF "$TEST_STRING" | head -n 1  2>/dev/null 1>&2
+    } || {
+        {
+            $PRINTF "$TEST_STRING" | busybox head -n 1  2>/dev/null 1>&2
+        } && {
+            HEAD="busybox head"
+        }
+    } || {
+        wrap_bad "head" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_find_present() {
+    {
+        find . -maxdepth 1 2>/dev/null 1>&2
+    } || {
+        {
+            busybox find . -maxdepth 1 2>/dev/null 1>&2
+        } && {
+            FIND="busybox find"
+        }
+    } || {
+        wrap_bad "find" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_zcat_present() {
+    {
+        zcat --help  2>/dev/null 1>&2
+    } || {
+        {
+            busybox zcat --help  2>/dev/null 1>&2
+        } && {
+            ZCAT="busybox zcat"
+        }
+    } || {
+        wrap_bad "zcat" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_awk_present() {
+    {
+        awk {print} "$TEST_FILE"  2>/dev/null 1>&2
+    } || {
+        {
+            busybox awk {print} "$TEST_FILE"  2>/dev/null 1>&2
+        } && {
+            AWK="busybox awk"
+        }
+    } || {
+        wrap_bad "awk" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_sed_present() {
+    {
+        sed -n 1p "$TEST_FILE"  2>/dev/null 1>&2
+    } || {
+        {
+            busybox sed -n 1p "$TEST_FILE"  2>/dev/null 1>&2
+        } && {
+            SED="busybox sed"
+        }
+    } || {
+        wrap_bad "sed" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_wc_present() {
+    {
+        wc "$TEST_FILE"  2>/dev/null 1>&2
+    } || {
+        {
+            busybox wc "$TEST_FILE"  2>/dev/null 1>&2
+        } && {
+            WC="busybox wc"
+        }
+    } || {
+        wrap_bad "wc" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_cut_present() {
+    {
+        $PRINTF "$TEST_STRING" | cut -d" " -f1  2>/dev/null 1>&2
+    } || {
+        {
+            $PRINTF "$TEST_STRING" | busybox cut -d" " -f1  2>/dev/null 1>&2
+        } && {
+            CUT="busybox cut"
+        }
+    } || {
+        wrap_bad "cut" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_sort_present() {
+    {
+        $PRINTF "$TEST_STRING" | sort  2>/dev/null 1>&2
+    } || {
+        {
+            $PRINTF "$TEST_STRING" | busybox sort 2>/dev/null 1>&2
+        } && {
+            SORT="busybox sort"
+        }
+    } || {
+        wrap_bad "sort" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_expr_present() {
+    {
+        expr linux : lin  2>/dev/null 1>&2
+    } || {
+        {
+            busybox expr linux : lin 2>/dev/null 1>&2
+        } && {
+            EXPR="busybox expr"
+        }
+    } || {
+        wrap_bad "expr" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_grep_present() {
+    {
+        echo "$TEST_STRING" | grep "$PATTERN" 2>/dev/null 1>&2
+    } || {
+        {
+            echo "$TEST_STRING" | busybox grep "$PATTERN" 2>/dev/null 1>&2
+        } && {
+            GREP="busybox grep"
+        }
+    } || {
+        wrap_bad "grep" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_test_present() {
+    {
+        test -n "$TEST_STRING" 2>/dev/null 1>&2
+    } || {
+        {
+            busybox test -n "$TEST_STRING" 2>/dev/null 1>&2
+        } && {
+            TEST="busybox test"
+        }
+    } || {
+        wrap_bad "test" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_dirname_present() {
+    {
+        dirname . 2>/dev/null 1>&2
+    } || {
+        {
+            busybox dirname . 2>/dev/null 1>&2
+        } && {
+            DIRNAME="busybox dirname"
+        }
+    } || {
+        wrap_bad "dirname" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_command_present() {
+    {
+        ## BusyBox does not support 'command'
+        command 2>/dev/null 1>&2
+    } || {
+        wrap_bad "command" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_read_present() {
+    {
+        ## BusyBox does not support 'read'
+        command -v read  2>/dev/null 1>&2
+    } || {
+        wrap_bad "read" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_readlink_present() {
+    {
+        command -v readlink 2>/dev/null 1>&2
+    } || {
+        {
+            busybox readlink -f "$TEST_FILE"  2>/dev/null 1>&2
+        } && {
+            READLINK="busybox readlink"
+        }
+    } || {
+        wrap_bad "readlink" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_tr_present() {
+    {
+        command -v tr 2>/dev/null 1>&2
+    } || {
+        {
+            $PRINTF "$TEST_STRING" | busybox tr ";" " "  2>/dev/null 1>&2
+        } && {
+            TR="busybox tr"
+        }
+    } || {
+        wrap_bad "tr" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_xargs_present() {
+    {
+        command -v xargs 2>/dev/null 1>&2
+    } || {
+        {
+            $LS | busybox xargs echo  2>/dev/null 1>&2
+        } && {
+            XARGS="busybox xargs"
+        }
+    } || {
+        wrap_bad "xargs" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_strings_present() {
+    {
+        command -v strings 2>/dev/null 1>&2
+    } || {
+        {
+            busybox strings "$TEST_FILE"  2>/dev/null 1>&2
+        } && {
+            STRINGS="busybox strings"
+        }
+    } || {
+        wrap_bad "strings" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_uniq_present() {
+    {
+        command -v uniq 2>/dev/null 1>&2
+    } || {
+        {
+            $PRINTF "$TEST_STRING" | busybox uniq  2>/dev/null 1>&2
+        } && {
+            UNIQ="busybox uniq"
+        }
+    } || {
+        wrap_bad "uniq" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_id_present() {
+    {
+        id 2>/dev/null 1>&2
+    } || {
+        {
+            busybox id 2>/dev/null 1>&2
+        } && {
+            ID="busybox id"
+        }
+    } || {
+        wrap_bad "id" "Not found"
+        DEPENDENCIES_PRESENT=0
+    }
+}
+
+check_commands_present() {
+    check_printf_present
+    check_eval_present
+    check_uname_present
+    check_cat_present
+    check_ls_present
+    check_head_present
+    check_find_present
+    check_zcat_present
+    check_awk_present
+    check_sed_present
+    check_wc_present
+    check_cut_present
+    check_sort_present
+    check_expr_present
+    check_grep_present
+    check_test_present
+    check_dirname_present
+    check_command_present
+    check_read_present
+    check_readlink_present
+    check_tr_present
+    check_xargs_present
+    check_strings_present
+    check_uniq_present
+    check_id_present
+}
+
+check_root_user() {
+    local effective_uid=$($ID | $GREP -o "uid=[0-9]*" | $CUT -d= -f2)
+
+    if [ $effective_uid -ne 0 ]
+    then
+        fatal "The script needs to be run as root."
+        fatal "Check the script usage with './check_ggc_dependencies --help'."
+        exit 1;
+    fi
+}
+
+check_script_dependencies() {
+    info "==========================Checking script dependencies=============================="
+    check_commands_present
+    if [ $DEPENDENCIES_PRESENT -eq 0 ]
+    then
+        fatal "The device is missing one or more of the script dependencies. Cannot proceed."
+        exit 1;
+    else
+        success "The device has all commands required for the script to run.\n"
+        check_root_user
+        check_ggc_dependencies
+    fi
+}
---
+++ greengrass-dependency-checker-GGCv1.9.0/check_sw_packages.sh
@@ -0,0 +1,173 @@
+JAVA_VERSION=""
+REQUIRED_PYTHON_VERSION="2.7"
+REQUIRED_NODEJS_VERSION="6.10"
+REQUIRED_JAVA_VERSION="8"
+
+no_op() {
+    :
+}
+
+################################################################################
+## Checks if Python is installed on the device and if installed, whether its
+## version meets the requirement for Greengrass lambdas.
+################################################################################
+check_python_version() {
+    local python_version_info
+    local python_version
+    local message="Could not find the binary 'python$REQUIRED_PYTHON_VERSION'.\n"
+    message="$message\nIf Python $REQUIRED_PYTHON_VERSION is installed on the"
+    message="$message device, name the binary 'python$REQUIRED_PYTHON_VERSION'"
+    message="$message and add its parent \ndirectory to the PATH environment variable."
+    message="$message Python $REQUIRED_PYTHON_VERSION is required to execute Python"
+    message="$message\nlambdas on Greengrass core."
+
+    {
+        ## Python reports the version to STDERR, so have to redirect STDERR to
+        ## STDOUT and not capture the output in a variable.
+        python$REQUIRED_PYTHON_VERSION --version >/dev/null 2>&1
+    } || {
+        wrap_warn "Python $REQUIRED_PYTHON_VERSION" "Not found"
+        add_to_dependency_warnings "$message"
+        return
+    }
+
+    python_version_info="$(python$REQUIRED_PYTHON_VERSION --version 2>&1)"
+    python_version="$(echo $python_version_info | $CUT -d" " -f2)"
+    if [ -n "$python_version_info" ]
+    then
+        wrap_good "Python version" "$python_version"
+    else
+        message="Failed to extract the Python version from the string:"
+        message="$message '$python_version_info'"
+        warn "$message"
+        add_to_warnings "$message"
+    fi
+}
+
+################################################################################
+## Checks if NodeJS is installed on the device and if installed, whether its
+## version is at least the minimum required version of NodeJS for Greengrass
+## lambdas.
+################################################################################
+check_nodejs_version() {
+    local node_version_info
+    local node_version
+    local message="Could not find the binary 'nodejs$REQUIRED_NODEJS_VERSION'.\n"
+    message="$message\nIf NodeJS $REQUIRED_NODEJS_VERSION or later is installed"
+    message="$message on the device, name the binary 'nodejs$REQUIRED_NODEJS_VERSION'"
+    message="$message and\nadd its parent directory to the PATH environment variable."
+    message="$message NodeJS $REQUIRED_NODEJS_VERSION or later is\nrequired to execute"
+    message="$message NodeJS lambdas on Greengrass core."
+
+    {
+        node_version_info="$(nodejs$REQUIRED_NODEJS_VERSION --version 2>/dev/null)"
+    } || {
+        wrap_warn "NodeJS $REQUIRED_NODEJS_VERSION" "Not found"
+        add_to_dependency_warnings "$message"
+        return
+    }
+
+    node_version="$(echo ${node_version_info#v})"
+    if [ -n "$node_version" ]
+    then
+        wrap_good "NodeJS version" "$node_version"
+    else
+        message="Failed to extract the NodeJS version from the string: '$node_version_info'"
+        warn "$message"
+        add_to_warnings "$message"
+    fi
+}
+
+################################################################################
+## Extracts the java version from the output of "java -version".
+################################################################################
+extract_java_version() {
+    local java_version_info="$1"
+
+    {
+        JAVA_VERSION="$(echo "$java_version_info" | $HEAD -n 1 | $GREP -o "[_0-9\.]*")"
+    } || {
+        message="Failed to extract the Java version from the string: '$java_version_info'"
+        warn "$message"
+        add_to_warnings "$message"
+    }
+
+}
+
+################################################################################
+## Checks if Java is installed on the device and if installed, whether its
+## version is at least the minimum required version of Java for Greengrass
+## lambdas.
+################################################################################
+check_java_version() {
+    local java_version_info
+    local java_version
+    local message="Could not find the binary 'java$REQUIRED_JAVA_VERSION'.\n"
+    message="$message\nIf Java $REQUIRED_JAVA_VERSION or later is installed on"
+    message="$message the device name the binary 'java$REQUIRED_JAVA_VERSION'"
+    message="$message and add its\nparent directory to the PATH environment"
+    message="$message variable. Java $REQUIRED_JAVA_VERSION or later is required"
+    message="$message to\nexecute Java lambdas on Greengrass core."
+
+    {
+        ## Java reports the version to STDERR, so have to redirect STDERR to
+        ## STDOUT and not capture the output in a variable.
+        java$REQUIRED_JAVA_VERSION -version >/dev/null 2>&1
+    } || {
+        wrap_warn "Java $REQUIRED_JAVA_VERSION" "Not found"
+        add_to_dependency_warnings "$message"
+        return
+    }
+
+    java_version_info="$(java$REQUIRED_JAVA_VERSION -version 2>&1)"
+    extract_java_version "$java_version_info"
+    if [ -n "$JAVA_VERSION" ]
+    then
+        wrap_good "Java version" "$JAVA_VERSION"
+    else
+        message="Failed to extract the Java version from the string: '$java_version_info'"
+        warn "$message"
+        add_to_warnings "$message"
+    fi
+}
+
+################################################################################
+## Checks if a command is present on the device.
+################################################################################
+check_if_command_present() {
+    local cmd="$1"
+    local message="The '$cmd' command was not found on the device. '$cmd'"
+    message="$message is required if the\nOver The Air(OTA) agent is used."
+
+    {
+        command -v $cmd 2>/dev/null 1>&2 && wrap_good "$cmd" "Present"
+    } || {
+        wrap_warn "$cmd" "Not found"
+        add_to_dependency_warnings "$message"
+    }
+}
+
+################################################################################
+## Checks if the software packages and commands required for the Over The
+## Air(OTA) agent are present on the device.
+################################################################################
+check_ota_agent_req() {
+    check_if_command_present "wget"
+    check_if_command_present "realpath"
+    check_if_command_present "tar"
+    check_if_command_present "readlink"
+    check_if_command_present "basename"
+    check_if_command_present "dirname"
+    check_if_command_present "pidof"
+    check_if_command_present "df"
+    check_if_command_present "grep"
+    check_if_command_present "umount"
+}
+
+check_sw_packages() {
+    info "----------------------------Commands and software packages--------------------------"
+    check_python_version
+    check_nodejs_version
+    check_java_version
+    check_ota_agent_req
+}
